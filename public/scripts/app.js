23700e9598b84e3863f30efe13dbb80f475f4346
kabitachatterjee <chatterjeekavita01@gmail.com>
2017-08-01 09:25:41 -0700

    added a scroll to top button

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -12,6 +12,11 @@ TripsIndexController.$inject = ['$http'];
 function TripsIndexController ($http, ModalService) {
     var vm = this;
     vm.today = new Date();
+    vm.dateOptions = {
+                      format: 'MM/dd/yyyy',
+                      minDate: vm.today,
+                      startingDay: 1
+                    };
               $http({
                     method: 'GET',
                     url: '/api/trips'
@@ -80,3 +85,21 @@ function TripsIndexController ($http, ModalService) {
 
 
 } // end of TripsIndexController
+
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Chrome, Safari and Opera
+    document.documentElement.scrollTop = 0; // For IE and Firefox
+}
