b0da2f8a839f6ff46ce6387e692ce1592ede39af
kabitachatterjee <chatterjeekavita01@gmail.com>
2017-08-01 09:40:27 -0700

    changed the narrative text on login and register pages

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -92,9 +92,9 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("myBtn").style.display = "block";
+        document.getElementById("scroll").style.display = "block";
     } else {
-        document.getElementById("myBtn").style.display = "none";
+        document.getElementById("scroll").style.display = "none";
     }
 }
 
