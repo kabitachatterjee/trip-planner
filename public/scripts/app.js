d08fa83403731ec652c9afa8d74ec9a89746b1da
kabitachatterjee <chatterjeekavita01@gmail.com>
2017-08-07 13:58:29 -0700

    removed the scroll function from the main application script and only added it for the index page

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -26,7 +26,6 @@ function TripsIndexController ($http, ModalService) {
                                 vm.trips = data.filter(function(el){
                                 return Date.parse(el.end_dt) > Date.now()
                                 });
-                                console.log(vm.trips);
                                 vm.length = vm.trips.length;
                                 vm.pastTrips = data.filter(function(el){
                                           return Date.parse(el.end_dt) < Date.now()
@@ -86,20 +85,3 @@ function TripsIndexController ($http, ModalService) {
 
 
 } // end of TripsIndexController
-
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("scroll").style.display = "block";
-    } else {
-        document.getElementById("scroll").style.display = "none"; }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-    document.body.scrollTop = 0; // For Chrome, Safari and Opera
-    document.documentElement.scrollTop = 0; // For IE and Firefox
-}
